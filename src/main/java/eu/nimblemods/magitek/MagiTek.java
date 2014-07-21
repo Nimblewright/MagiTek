757c4aa7b9428a94534b0037db594318a856728c
Simon <simonvanraaij@gmail.com>
2014-07-21 23:17:27 +0200

    First commit

---
+++ src/main/java/eu/nimblemods/magitek/MagiTek.java
@@ -0,0 +1,31 @@
+package eu.nimblemods.magitek;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "MagiTek", name = "MagiTek", version = "1.7.2-0.1a")
+public class MagiTek
+{
+    @Mod.Instance("MagiTek")
+    public static MagiTek instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e)
+    {
+
+    }
+}
